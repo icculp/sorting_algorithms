5a9b8231b1e080bc519d2638f54dc44850d42110
Ian Culp <icculp@gmail.com>
2020-06-14 19:05:30 +0000

    Swap only when indexes different, check for NULL

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,16 +10,22 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, t, min;
 
+	if (array == NULL)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min])
 				min = j;
-		t = array[min];
-		array[min] = array[i];
-		array[i] = t;
-		print_array(array, size);
+		if (min != i)
+		{
+			t = array[min];
+			array[min] = array[i];
+			array[i] = t;
+			print_array(array, size);
+		}
 	}
 
 }
